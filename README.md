43cbd0f1f00614a247eb87c430e332a02e8ea379
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-14 20:15:32 +0600

    bug fixed clear previous search result

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-
 2. after typing something on the search textbox, you should be able to press enter to get search result ------ok
 3. spinner is always running. should be displayed at the time of data loading------ok
 4. If you search for 'iphone', check the api, you should get a lot of data but the UI displays only one phone ----ok
-5. Search is not clearing previous results
+5. Search is not clearing previous results ---ok
 6. picture of the phones are not showing ---- ok
 7. Show All button should be hidden after all data is loaded 
 8. Load phone details doesn't display anything
--- js/app.js
+++ js/app.js
@@ -10,14 +10,14 @@ const displayPhones = (phones, dataLimit) =>{
     // phonesContainer.textContent = '';
     // display 10 phones only 
     const showAll = document.getElementById('show-all');
-    console.log(phones.length);
-    if(dataLimit && phones.length > 10) {
+    if(dataLimit===10 && phones.length > 10) {
         phones = phones.slice(0, 10);
         showAll.classList.remove('d-none');
     }
     else{
         showAll.classList.add('d-none');
     }
+    console.log(phones);
     
 
     // display no phones found
@@ -29,10 +29,10 @@ const displayPhones = (phones, dataLimit) =>{
         noPhone.classList.add('d-none');
     }
     // display all phones
+    phonesContainer.innerHTML=''
     phones.forEach(phone =>{
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
-        phoneDiv.innerHTML='';
         phoneDiv.innerHTML = `
         <div class="card p-4">
             <img src="${phone.image}" class="card-img-top" alt="...">
@@ -44,6 +44,7 @@ const displayPhones = (phones, dataLimit) =>{
             </div>
         </div>
         `;
+        
         phonesContainer.appendChild(phoneDiv);
     });
     // stop spinner or loader
@@ -108,4 +109,4 @@ const displayPhoneDetails = phone =>{
     `
 }
 
-loadPhones('apple');
+loadPhones('apple',10);
